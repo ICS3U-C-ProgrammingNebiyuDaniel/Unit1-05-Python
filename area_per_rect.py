c74e3cfe11090c25327c5a3b6a278c48a8bdb762
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-09-17 17:52:51 +0000

    first part done

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: Nebiyu Daniel
+# Date: Sep. 17, 2025
+# This program calculates the area & perimeter of a 5cm X 3cm rectangle.
+# Copyright (c) 2025 Nebiyu Daniel
+
+
+def main():
+    print("For a rectangle with length 9cm and width 2cm:")
+    print()
+    print("The area is: {}cm^2".format(9 * 2))
+    print("The perimeter is: {}cm".format(2 * (9 + 2)))
+
+
+if __name__ == "__main__":
+    main()
